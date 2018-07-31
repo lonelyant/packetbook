471004b71a0960dd11c52d4b465a3400f77e6ddf
LonelyAnt <lonelyant404@gmail.com>
2018-07-31 11:30:08 +0800

    1.项目初始化

---
+++ src/main/java/com/zcx/packetbook/PacketbookApplication.java
@@ -0,0 +1,12 @@
+package com.zcx.packetbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacketbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacketbookApplication.class, args);
+	}
+}
